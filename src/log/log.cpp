75faeb35b06789722fea9aaa7db7b69b508d455b
Nicolas Schurando <nicolas@sitronlabs.com>
2023-09-02 00:31:25 +0200

    Improved log formatting.

--- src/log/log.cpp
+++ src/log/log.cpp
@@ -1,8 +1,10 @@
 /* Self header */
 #include "log.h"
 
-/* Arduino libraries */
+#if R8A
+/* Pico libraries */
 #include <tusb.h>
+#endif
 
 /* Config */
 #include "../../cfg/config.h"
@@ -23,23 +25,23 @@ int log_setup(void) {
 #if R4J
     m_uart_library = &Serial;
 #elif R8A
-    m_uart_library = &Serial;  // Temp
-    Serial.begin(115200);      // Temp
-    pinMode(8, INPUT_PULLUP);
-    if (digitalRead(8) == LOW) {
-        uint32_t t = millis();
-        while (1) {
-            if (millis() - t >= 10000) {
-                break;
-            } else if (tud_cdc_connected()) {
-                delay(500);
-                break;
-            }
-        }
-    }
+    m_uart_library = &Serial1;
+    Serial1.begin(115200);
+    // m_uart_library = &Serial;  // Temp
+    // Serial.begin(115200);      // Temp
+    // pinMode(8, INPUT_PULLUP);
+    // if (digitalRead(8) == LOW) {
+    //     uint32_t t = millis();
+    //     while (1) {
+    //         if (millis() - t >= 10000) {
+    //             break;
+    //         } else if (tud_cdc_connected()) {
+    //             delay(500);
+    //             break;
+    //         }
+    //     }
+    // }
     Serial.printf("Hello world!\r\n");
-
-    // m_uart_library = &Serial1;
 #else
 #error Invalid hardware version
 #endif
@@ -56,15 +58,14 @@ int log_setup(void) {
  * @param[in] format The message formatter string potentially followed by the variables names to replace.
  */
 int log_write(const enum log_level level, const char *const file, const unsigned int line, const char *const format, ...) {
+    int res;
 
     /* Ensure output peripheral has been configured */
     if (m_uart_library == NULL) {
         return -EINVAL;
     }
 
-    (void)file;
-    (void)line;
-
+    /* Ensure level is valid */
     if (level != LOG_LEVEL_TRACE &&
         level != LOG_LEVEL_DEBUG &&
         level != LOG_LEVEL_INFO &&
@@ -73,19 +74,28 @@ int log_write(const enum log_level level, const char *const file, const unsigned
         return -EINVAL;
     }
 
-    /* Discard messages that do not meet verbosity level */
-    // if (level < m_verbosity) return 0;
+    /* Add timestamp, level, file and line  */
+    (void)file;
+    (void)line;
+    const char *const levels[] = {
+        [LOG_LEVEL_TRACE] = "trc",
+        [LOG_LEVEL_DEBUG] = "dbg",
+        [LOG_LEVEL_INFO] = "nfo",
+        [LOG_LEVEL_WARNING] = "wrn",
+        [LOG_LEVEL_ERROR] = "err",
+    };
+    res = sprintf(&m_buffer[0], "[%" PRIu32 "] [%s] ", millis(), levels[level]);
+    if (res < 0) {
+        return -1;
+    }
 
     /* Add content */
     va_list args;
     va_start(args, format);
-    vsnprintf(m_buffer, CONFIG_LOG_ENTRY_MAX_LENGTH, format, args);
+    vsnprintf(&m_buffer[res], CONFIG_LOG_ENTRY_MAX_LENGTH - res, format, args);
     va_end(args);
 
-    ///* Output to semihosting */
-    // static const char level_indicator[] = {'t', 'd', 'i', 'w', 'e'};
-    // printf(" [%c] [%s:%d] %s\n", level_indicator[level], m_basename(file), line, m_buffer);
-
+    /* Output to uart */
     m_uart_library->println(m_buffer);
     m_uart_library->flush();
 
