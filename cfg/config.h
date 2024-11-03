7fbf8f07ada14895be6ac6f8f97996402a9b7428
Nicolas Schurando <nicolas@sitronlabs.com>
2024-11-03 20:11:51 +0100

    Reduced idle timeout to one minute but increased wake sensitivity.

--- cfg/config.h
+++ cfg/config.h
@@ -9,9 +9,9 @@
 
 /* Accelerometer config */
 #define CONFIG_ACCEL_SAMPLE_PERIOD 100               //!< In milliseconds
-#define CONFIG_ACCEL_IDLE_TIME 180000                //!< In milliseconds
+#define CONFIG_ACCEL_IDLE_TIME 60000                 //!< In milliseconds
 #define CONFIG_ACCEL_IDLE_ACCELERATION_TRESHOLD 0.1  //!< In g, relative to mediam magnitude.
-#define CONFIG_ACCEL_WAKE_ACCELERATION_TRESHOLD 1.0  //!< In g, relative to mediam magnitude.. Has to be lower than the configured full scale.
+#define CONFIG_ACCEL_WAKE_ACCELERATION_TRESHOLD 0.5  //!< In g, relative to mediam magnitude.. Has to be lower than the configured full scale.
 #define CONFIG_ACCEL_FALL_ACCELERATION_TRESHOLD 0.3  //!< In g, absolute.
 
 /* Buttons config */
