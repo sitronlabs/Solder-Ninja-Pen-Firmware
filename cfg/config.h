7097b155546b53a11955d822fe1bb1160e33663c
Nicolas Schurando <nicolas@sitronlabs.com>
2025-06-05 17:25:41 +0200

    Cleaned up config comments.

--- cfg/config.h
+++ cfg/config.h
@@ -2,15 +2,15 @@
 #define CONFIG_H
 
 /* App config */
-#define CONFIG_APP_TARGET_MIN 0                //!< Minimum target temperature in degrees celsius
-#define CONFIG_APP_TARGET_MAX_SAFE 350         //!< Maximum target temperature in degrees celsius
-#define CONFIG_APP_TARGET_MAX_BOOST 400        //!< Maximum target temperature in degrees celsius
-#define CONFIG_APP_BOOST_DURATION_LIMIT 30000  //!< Amount of time after which the boost will end, in milliseconds
+#define CONFIG_APP_TARGET_MIN 0                //!< Minimum target temperature (in degrees celsius).
+#define CONFIG_APP_TARGET_MAX_SAFE 350         //!< Maximum target temperature (in degrees celsius).
+#define CONFIG_APP_TARGET_MAX_BOOST 400        //!< Maximum target temperature in temporary boost (in degrees celsius).
+#define CONFIG_APP_BOOST_DURATION_LIMIT 30000  //!< Amount of time after which the boost will end (in milliseconds).
 
 /* Accelerometer config */
-#define CONFIG_ACCEL_IDLE_TIME 10000                   //!< In milliseconds
-#define CONFIG_ACCEL_IDLE_ACCELERATION_TRESHOLD 0.160  //!< In g, after high pass filter.
-#define CONFIG_ACCEL_FALL_ACCELERATION_TRESHOLD 0.336  //!< In g, absolute.
+#define CONFIG_ACCEL_IDLE_TIME 10000                   //!< Time after which no movement of the device will be interpreted as inactivity (in milliseconds).
+#define CONFIG_ACCEL_IDLE_ACCELERATION_TRESHOLD 0.160  //!< Threshold under which acceleration will not be registered as movement (in g, after high pass filter).
+#define CONFIG_ACCEL_FALL_ACCELERATION_TRESHOLD 0.336  //!< Threshold under which acceleration will be considered as free-fall (in g, absolute).
 
 /* Buttons config */
 #define CONFIG_BUTTONS_PRESS_SHORT_DURATION 30                    //!<
@@ -19,8 +19,8 @@
 #define CONFIG_BUTTONS_COMBINED_PRESS_LONG_REPEAT_DURATION 500    //!<
 
 /* Dislay config */
-#define CONFIG_DISPLAY_WIDTH 96   //!< Horizontal size of the display, in pixels.
-#define CONFIG_DISPLAY_HEIGHT 16  //!< Vertical size of the display, in pixels.
+#define CONFIG_DISPLAY_WIDTH 96   //!< Horizontal size of the display (in pixels).
+#define CONFIG_DISPLAY_HEIGHT 16  //!< Vertical size of the display (in pixels).
 
 /* Orders config */
 #define CONFIG_COMMAND_LENGTH_LIMIT 256  //!<
@@ -34,8 +34,8 @@
 #define CONFIG_TIP_READ_TIMEOUT 2000      //!< Maximum time after which no valid temperature readins will lead to considering the tip is disconnected (in milliseconds).
 
 /* User interface config */
-#define CONFIG_UI_SPLASH_DURATION 1500  //!< Duration of the splash screen, in milliseconds.
-#define CONFIG_UI_INFO_DURATION 1500    //!< Duration of each information screen, in milliseconds.
-#define CONFIG_UI_ADJUST_DURATION 1000  //!< Amount of time the target temperature is displayed before reverting to the monitor temperature, in milliseconds.
+#define CONFIG_UI_SPLASH_DURATION 1500  //!< Duration of the splash screen (in milliseconds).
+#define CONFIG_UI_INFO_DURATION 1500    //!< Duration of each information screen (in milliseconds).
+#define CONFIG_UI_ADJUST_DURATION 1000  //!< Amount of time the target temperature is displayed before reverting to the measured temperature (in milliseconds).
 
 #endif
